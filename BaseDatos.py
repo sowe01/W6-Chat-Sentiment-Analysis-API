5ea91376efeabc23edc389a139c3e5f8b77952ba
sowe01 <sowe01@gmail.com>
2020-02-25 07:31:28 +0100

    sigo igual..

--- BaseDatos.py
+++ BaseDatos.py
@@ -1,21 +1,23 @@
 from pymongo import MongoClient
 from bson.json_util import dumps
+import pandas as pd
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import re
 from flask import Flask, request
+from errorHandler import jsonErrorHandler
+
 # conexión a Mongo:
 client = MongoClient("mongodb://localhost:27017/")
 
 # Creo una base de datos y dos colecciones
 db = client.get_database('chat')
 user_coll = db["users"]
-chat_coll = db["chats"]
 
 
 # funcion para crear el usuario
 
-
+@jsonErrorHandler
 def creauser(name):
     if len(list(user_coll.find({}, {"idUser": 1}))) == 0:
         new_id = 1
@@ -37,7 +39,104 @@ def id_check():
     print(id_number)
     return id_number
 
+#comprueba si el usuario existe para evitar duplicados
+def checkName(name, tipo="name"):
+    q = {"userName": {"$exists": True}}
+    query = user_coll.find(q, projection={"userName": 1, "_id": 0})
+    findname = [name for i in query for name in i.values()]
 
+    if name in findname:
+        if tipo == "name":
+            return f"{name} ya existe en la base de datos. Por favor elige otro nombre."
+        elif tipo == "conversation":
+            return "OK"
+    else:
+        if tipo == "name":
+            return "OK"
+        elif tipo == "conversation":
+            return f"{name} NO existe en la base de datos"
+
+
+
+
+@jsonErrorHandler
+def getUserSentiment(name):
+    """
+    Get sentimientos de un usuario --> API @app.route("/get/sentiment/user/<name>")
+    """
+    sia = SentimentIntensityAnalyzer()
+    name = name.capitalize()
+    q = {"userName": name}
+    query = user_coll.find(q, projection={"idUser": 0, "idMessage": 1})
+    if not query:
+        raise ValueError("No se han encontrado usuarios")
+
+    lst = list(query)
+    count = 0
+    df = pd.DataFrame(columns=["neg", "neu", "pos", "compound"]).T
+    for elem in lst:
+        for idMessage in elem.values():
+            sent = sia.polarity_scores(idMessage)
+            df[count] = sent.values()
+            count += 1
+    df = df.T
+    negative = df.neg.mean()
+    neutral = df.neu.mean()
+    positive = df.pos.mean()
+
+    return {
+        "user_name": name,
+        "negative": (negative * 100).round(2),
+        "neutral": (neutral * 100).round(2),
+        "positive": (positive * 100).round(2),
+    }
+
+@jsonErrorHandler
+def getAllSentiment():
+    """
+    Get sentimientos de todos los usuarios --> API @app.route("/get/sentiments")
+    """
+    df = getMySentMatrix()
+    df_json = df.to_json(orient="records")
+
+    return df_json
+
+def getMySentMatrix():
+    """
+    Devuelve la matriz de sentimientos de los usuarios registrados. 
+    Útil para analizar sentimientos y realizar recomendaciones.
+    """
+    q = {}
+    query_quote = user_coll.find(q, projection={"idUser": 0, "idMessage": 1})
+    query_user = user_coll.find(q, projection={"idUser": 0, "name": 1})
+
+    lst_quote = list(query_quote)
+    lst_user = list(query_user)
+
+    count = 0
+    df = pd.DataFrame(columns=["neg", "neu", "pos", "compound"]).T
+    sia = SentimentIntensityAnalyzer()
+    for elem in lst_quote:
+        for idMessage in elem.values():
+            sent = sia.polarity_scores(idMessage)
+            df[count] = sent.values()
+            count += 1
+    df = df.T
+
+    names = [user for elem in lst_user for user in elem.values()]
+
+    df["name"] = names
+
+    df = (
+        df.groupby("name")
+        .mean()
+        .round(4)
+        .sort_values("compound", ascending=False)
+        .reset_index()
+    )
+
+    return df
+'''
 # realiza la media de los sentimientos y devuelve el resultado final
 
 def getFinalMetric(chat):
@@ -73,3 +172,4 @@ def getSentimentReport(chat):
             text)+1)] = (text['userName']+': '+text['text']), (sid.polarity_scores(text['text']))
     scores['Complete chat sentiment Metric'] = getFinalMetric(chat)
     return scores
+'''
--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from pymongo import MongoClient
-from BaseDatos import creauser, getSentimentReport, getFinalMetric, chat_coll, user_coll
+from BaseDatos import creauser, user_coll, checkName, getUserSentiment, getAllSentiment
 from bson.json_util import dumps
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import json
@@ -18,9 +18,9 @@ def users(name):
 def crearchat(chat):
     name = str(request.forms.get("name"))
     text = str(request.forms.get("text"))
-    new_id = chat_coll.distinct("idUser")[-1] + 1
-    new_idm = chat_coll.distinct("idMessage")[-1] + 1
-    new_idc = chat_coll.distinct("idChat")[-1] + 1
+    new_id = user_coll.distinct("idUser")[-1] + 1
+    new_idm = user_coll.distinct("idMessage")[-1] + 1
+    new_idc = user_coll.distinct("idChat")[-1] + 1
     new_user = {
         "idUser": new_id,
         "userName": name,
@@ -28,7 +28,7 @@ def crearchat(chat):
         "idChat": new_idc,
         'text': text
     }
-    chat_coll.insert_one(new_user)
+    user_coll.insert_one(new_user)
     return json.dumps({new_user: 'hecho'})
 
 
@@ -40,22 +40,26 @@ def getUsers():
 # ver todos los mensajes de la base de datos
 @app.route("/messages")
 def getChat():
-    return dumps(chat_coll.find({}, {"text": 1, '_id': 0}))
+    return dumps(user_coll.find({}, {"text": 1, '_id': 0}))
 
 # ver todas las conversaciones
 @app.route("/chatid")
 def getConversations():
-    return dumps(chat_coll.find({}, {'idChat': 1, 'text': 1, '_id': 0}))
+    return dumps(user_coll.find({}, {'idChat': 1, 'text': 1, '_id': 0}))
 
 
 # analisis de sentimientos
+@app.route("/get/sentiments")
+def getSentimentAll():
+    return getAllSentiment()
+'''
 @app.route("/chat/<idChat>/sentiment")
 def sentimentReport(idChat):
     chat = dumps(chat_coll.find({'idChat': int(idChat)}, {
                  'userName': 1, 'text': 1, '_id': 0}))
     report = getSentimentReport(chat)
     return report
-
+'''
 # sistema de recomendaciones
 @app.route("/users")
 def getAllUsers():
